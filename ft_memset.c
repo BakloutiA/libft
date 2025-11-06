3cd0b1591b362cc778b6d3f607920c1e892c8448
BakloutiA <bkloutiaymen@gmail.com>
2025-11-06 10:28:46 +0100

    Initial commit

---
+++ ft_memset.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+	unsigned char	ch;
+
+	p = (unsigned char *)b;
+	ch = c;
+	while (len--)
+	{
+		*p = ch;
+		p++;
+	}
+	return (b);
+}
---
+++ ft_strchr.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+
+
+char	*ft_strchr(const char *str, int c)
+{
+	int				i;
+	unsigned char	ch;
+
+	i = 0;
+	ch = c;
+	if (ch == '\0')
+	{
+		i = ft_strlen(str);
+		return ((char *)str + i++);
+	}
+	while (str[i])
+	{
+		if (str[i] == ch)
+			return ((char *)str + i);
+		i++;
+	}
+	return (NULL);
+}
